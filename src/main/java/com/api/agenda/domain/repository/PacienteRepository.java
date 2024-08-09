a4cc331b11994b59218549dd86a4f23496a642cb
mathe <matheus.felipe.bota.10@gmail.com>
2024-08-09 15:40:36 -0400

    feat nova coluna

--- src/main/java/com/api/agenda/domain/repository/PacienteRepository.java
+++ src/main/java/com/api/agenda/domain/repository/PacienteRepository.java
@@ -1,14 +1,24 @@
 package com.api.agenda.domain.repository;
 
 import com.api.agenda.domain.entity.Paciente;
+import com.api.agenda.domain.entity.PacienteAgenda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Optional<Paciente> findByCpf(String  cpf);
 
-    
+    @Query("SELECT p FROM Paciente p WHERE p.cpf = ?1")
+    Optional<Paciente> getByCpf(String cpf);
+
+    @Query(value = "SELECT * FROM Paciente p WHERE p.cpf = ?1", nativeQuery = true)
+    Optional<Paciente> getByCpfNative(String cpf);
+
+    @Query(value = "select p.nome, p.cpf, a.horario, a.descricao from paciente p, agenda a where p.id = a.paciente_id", nativeQuery = true)
+    List<PacienteAgenda> findAllAgenScheduling();
 }
---
+++ src/main/java/com/api/agenda/domain/service/UsuarioService.java
@@ -0,0 +1,42 @@
+package com.api.agenda.domain.service;
+
+import com.api.agenda.domain.entity.Usuario;
+import com.api.agenda.domain.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UsuarioService implements UserDetailsService {
+
+    private final UsuarioRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = repository.findByUsuario(username).orElseThrow(()
+                -> new UsernameNotFoundException("User Not found"));
+        return new User(usuario.getUsuario(), usuario.getSenha(), new ArrayList<>());
+    }
+
+    public List<Usuario> getAll(){
+        return repository.findAll();
+    }
+
+    public Usuario save(Usuario usuario){
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        return repository.save(usuario);
+    }
+
+
+}
